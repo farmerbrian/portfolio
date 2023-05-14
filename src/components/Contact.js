6f83b2b561007821ca7bfbc1076e26bd15884000
farmerbrian <nichols.brian.d@gmail.com>
2023-05-14 18:56:03 -0500

    Updated color schemes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,11 @@
+// import Linkedin from '../assets/linkedin.svg';
+
 function Contact() {
 	return (
 		<div className="Contact">
 			<h2>Contact Me</h2>
 			<h4>Email: nichols.brian.d@gmail.com</h4>
+			{/* <img className="Icons" src={Linkedin} alt="Linkedin Logo"></img> */}
 		</div>
 	);
 }
