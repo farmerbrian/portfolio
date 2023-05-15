24691cce371e4fd2fb56931888b9e6c911de8b3e
farmerbrian <nichols.brian.d@gmail.com>
2023-05-14 21:50:21 -0500

    Changed About text

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,9 @@ function About() {
 		<div className="About">
 			<h2>About Me</h2>
 			<p>
-				Hello there, my name is Brian Nichols and I’ve been in the IT
-				field for 14 years. Over the years I’ve dabbled in coding
-				software but have recently put a major emphasis on learning
-				languages such as JavaScript and React. Take a look below to
-				see some of the projects I’ve been working on and please feel
-				free to reach out to me if you have any questions.
+				Hey there, my name is Brian Nichols and I’ve been in the IT
+				field for over 14 years. But now I've been learning
+				development. Check out some of my projects below.
 			</p>
 		</div>
 	);
